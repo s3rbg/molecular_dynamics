619f6bf2aef8cb10332b0d265f56fc983977342c
duvidG <56431707+duvidG@users.noreply.github.com>
2021-11-22 16:45:29 +0100

    Add files via upload

--- Simulation.py
+++ Simulation.py
@@ -5,9 +5,20 @@ Created on Sun Oct 24 16:01:22 2021
 
 @author: dgiron
 """
+# Input imports
+from read_parameters.read_data_file import read_data_txt
+from read_parameters.read_data_gui import create_gui
+from read_parameters.check_input_errors import check_type
 
-from read_parameters.Input_file import read_data_txt
-from read_parameters.read_data import create_gui
+# Initialization imports
+from init.initialize import initialize
+
+# Potential calculation imports
+
+# ODE solving imports
+from solve_ode.ode_algorithms import verlet, velocity_verlet, leap_frog
+
+# Other imports
 from common_modules.imports import *
 from common_modules.units_dicts import *
 from common_modules.errors import InputError
@@ -15,11 +26,11 @@ from common_modules.errors import InputError
 #Total simulation time. Añadir opciones para los 
 # demas parametros
 
-# uses eV, ang, g/cm3
+# uses eV, ang, g/cm3, K
 
-k = 8.62e-5 # eV
+k = 8.62e-5 # eV * K
 
-how_many_units = 4 # starting from end
+how_many_units = 4 # starting from end. How many elements of the list are units
 
 class simulation():
     def __init__(self, data_file):
@@ -31,7 +42,7 @@ class simulation():
         units = df.iloc[:, -how_many_units:]
         try:
             self.density = float(df['density'].iloc[0]) * density_units[units.iloc[:, 0].item()]
-            self.n_fcc = int(df['number_of_units'].iloc[0]) # IF N is float uses closest integer
+            self.n_cells = int(df['number_of_units'].iloc[0]) # IF N is float uses closest integer
             
             self.cell_type = df['cell_type'].iloc[0]
             self.potential_type = df['potential_type'].iloc[0]
@@ -42,28 +53,17 @@ class simulation():
             self.delta_t = float(df['delta_t'].iloc[0])
             
             # Parameters Verlet-Lennard
-            if self.potential_type == 'lennard-jones' and self.algo_ode == 'verlet':
+            if self.potential_type == 'lennard-jones':
                 self.sigma = float(df['sigma'].iloc[0]) * distance_units[units.iloc[:, 1].item()]
                 self.epsilon =  float(df['epsilon'].iloc[0]) * energy_units[units.iloc[:, 2].item()]
     
-                self.cutoff_distance = float(df['cutoff_distance'].iloc[0])
-                self.cutoff_list = float(df['cutoff_list'].iloc[0])
+            self.cutoff_distance = float(df['cutoff_distance'].iloc[0])
+            self.cutoff_list = float(df['cutoff_list'].iloc[0])
             a = float(df['temperature'].iloc[0])
         
         except ValueError:
-            check_float = [i for i, j in zip(keywords, default_data) if type(j) == float]
-            check_int = [i for i, j in zip(keywords, default_data) if type(j) == int]
-            for i in check_float:
-                try:
-                    float(df[i].iloc[0])
-                except:
-                    raise InputError('Wrong type detected in "{}". Expected float, but got string'.format(i))
-                    
-            for i in check_int:
-                try:
-                    int(df[i].iloc[0])
-                except:
-                    raise InputError('Wrong type detected in "{}". Expected int, but got string'.format(i))
+            check_type(df)
+            
         
         self.temperature = float(self.transform_to_kelvin(float(df['temperature'].iloc[0]), df['temperature_units'].iloc[0]))      
         self.n_at = number_atoms_cell[self.cell_type]
@@ -74,9 +74,9 @@ class simulation():
         self.lattice_constant = self.get_constant()
          
     def transform_to_kelvin(self, temp, unit):
-        if unit == 'celsius':
+        if unit == 'celsius' or unit == 'c':
             return 273.15 + temp
-        elif unit == 'fahrenheit':
+        elif unit == 'fahrenheit' or unit == 'f':
             return (temp - 32) * 5/9 + 273.15
         else:
             return temp
@@ -93,9 +93,25 @@ class simulation():
         """
         return (self.n_at/self.reduced_density) ** 1/3
 
+
+    def simulate(self):
+        
+        # Initialize cosas
+        
+        positions, velocities = initialize(self.reduced_temperature, self.n_cells, self.sigma, self.lattice_constant)
+
+        # Solve ode
+        # step = 0
+        
+        # if self.algo_ode == 'velocity-verlet':
+        #     f = velocity_verlet
+        # while step <= self.tot_t:
+        #     positions, velocities, accelerations = f(positions, velocities, accelerations, self.n_at, self.delta_t, self.cutoff_list)
+        #     step = step + self.delta_t
+    
     def get_parameters(self):
         
-        return {'density':self.density, 'number_of_units':self.n_fcc, 'epsilon':self.epsilon, 
+        return {'density':self.density, 'number_of_units':self.n_cells, 'epsilon':self.epsilon, 
                 'temperature':self.temperature, 'cell_type':self.cell_type, 'potential_type':self.potential_type,
                 'algo_ode':self.algo_ode, 'tot_t':self.tot_t, 'delta_t':self.delta_t, 'sigma':self.sigma,
                 'cutoff_distance':self.cutoff_distance, 'cutoff_list':self.cutoff_list, 'vel_dist':self.vels_dist, 
