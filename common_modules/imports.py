6ad3e017e17b0eec82ef05a656a3e9f1aeb86bb1
duvidG <56431707+duvidG@users.noreply.github.com>
2022-01-09 16:33:29 +0100

    Add files via upload

--- common_modules/imports.py
+++ common_modules/imports.py
@@ -1,13 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from scipy.optimize import curve_fit as cf
 import math
-from scipy.stats import chisquare
-from scipy.special import gamma
-from uncertainties import ufloat, unumpy
-from uncertainties.umath import *
-from uncertainties.unumpy import nominal_values as nv
-from uncertainties.unumpy import std_devs as st
-import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import os
+import pandas as pd
--- common_modules/units_dicts.py
+++ common_modules/units_dicts.py
@@ -6,7 +6,7 @@ Created on Sat Oct 30 17:23:12 2021
 @author: dgiron
 """
 
-distance_units_upp = {"m": 10**9,  "mm": 10**6,  "cm": 10**7,  "nm": 1,  
+distance_units_upp = {"nm": 1, "m": 10**9,  "mm": 10**6,  "cm": 10**7,  
                   "microm": 10**3, 'Angstrom': 0.1, 'Bohr rad': 0.0529177}
 
 density_units_upp = {"m**-3": 10**-6, "cm**-3": 1}
@@ -15,19 +15,21 @@ energy_units_upp = {'J':1/1.6e-19, 'erg': 6.242e+11, 'eV':1, 'cal': 261319369518
 
 number_atoms_cell_upp = {'FCC': 4, 'BCC': 2, 'SC': 1}
 
-temperature_units_upp = ('Celsius', 'C', 'K', 'F', 'Fahrenheit', 'Kelvin')
+temperature_units_upp = ('Kelvin', 'Celsius', 'C', 'K', 'F', 'Fahrenheit')
+
+temperature_units_special_upp = ('Kelvin', 'Celsius', 'Fahrenheit')
 
 ode_algos_upp = ('Velocity-Verlet', 'Verlet', 'Leap-Frog')
 potential_types_upp = ('Lennard-Jones double shifted', 'Lennard-Jones') # (...)
 vels_dist_upp = ('M-B', 'otro')
 
 keywords = ('density', 'number_of_units', 'epsilon', 'temperature',
-                   'cell_type', 'potential_type', 'algo_ode', 'tot_t', 'delta_t', 'sigma',
-                   'cutoff_distance', 'cutoff_list', 'velocities_dist', 'directory', 'density_units', 'sigma_unit', 
-                   'epsilon_units', 'temperature_units')
+            'cell_type', 'potential_type', 'algo_ode', 'tot_t', 'delta_t', 'sigma',
+            'cutoff_distance', 'cutoff_list', 'velocities_dist', 'directory', 'density_units', 
+            'sigma_unit', 'epsilon_units', 'temperature_units')
 
-default_data = [0.09e-3, 3, 1e-18, 300, 'fcc', 'lennard-jones double shifted', 'velocity-verlet',
-                        1000, 0.001, 0.231, 2.5, 2.7, 'm-b', 'output', 'kg/m**3', 'nm', 'j', 'k']
+default_data = [2.1290321e28, 3, 1.654438e-21, 86.4956, 'fcc', 'lennard-jones double shifted', 'velocity-verlet',
+                        100, 0.005, 0.341, 2.5, 2.7, 'm-b', 'output', 'm**-3', 'nm', 'j', 'k']
 
 distance_units =  {k.lower(): v for k, v in distance_units_upp.items()}
 
